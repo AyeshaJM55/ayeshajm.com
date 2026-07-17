0ab19b6dba17ca581501dfb3ff2a5c91cc09fe70
AyeshaJM55 <ayesha.jm55@gmail.com>
2026-07-17 04:43:24 +0000

    first commit

---
+++ project/aspect-ratio.tsx
@@ -0,0 +1,11 @@
+{
+  "version": 1,
+  "asset_id": "7f1cffb3-e55c-473f-a173-ab78b43833ac",
+  "project_id": "dba07ca8-c910-4c28-967b-8bad0b07d837",
+  "url": "/__l5e/assets-v1/7f1cffb3-e55c-473f-a173-ab78b43833ac/hero5.jpg",
+  "r2_key": "a/v1/dba07ca8-c910-4c28-967b-8bad0b07d837/7f1cffb3-e55c-473f-a173-ab78b43833ac/hero5.jpg",
+  "original_filename": "hero5.jpg",
+  "size": 91607,
+  "content_type": "image/jpeg",
+  "created_at": "2026-07-16T13:33:40Z"
+}
